b425c2bb706b65e9f09af6333ff08acd1deee4c8
AndSak <andriysak80@gmail.com>
2022-02-15 17:09:21 +0200

    feature: add PostList

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React, { useState } from 'react';
 import Counter from './components/Counter';
 import ClassCounter from './components/ClassCounter'
+import PostItem from './components/PostItem'
+
+
+import './styles/App.css';
+import PostList from './components/PostList';
 
 function App() {
   const [value, setValue] = useState('word');
 
-  return (
-    <div>
-      <Counter/>
-      <ClassCounter/>
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript 1', body: 'Description' },
+    { id: 2, title: 'JavaScript 2', body: 'Description' },
+    { id: 3, title: 'JavaScript 3', body: 'Description' },
+  ]);
 
-      <h2>{value}</h2>
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Java 11', body: 'Desc' },
+    { id: 2, title: 'Java 22', body: 'Desc' },
+    { id: 3, title: 'Java 33', body: 'Desc' },
+  ]);
+
+  return (
+    <div className='App'>
+      <PostList posts={posts} title='Order list 1' />
+      <PostList posts={posts2} title='Order list 2' />
+
+      < Counter />
+      < ClassCounter />
+      <h2> {value} </h2>
       <input
         type="input name"
         value={value}
